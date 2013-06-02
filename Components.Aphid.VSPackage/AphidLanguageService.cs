e5eaf2a4fd95836bdcabae58fba79afcb0fd1ae1
John Leitch <john.leitch5@gmail.com>
2013-06-02 07:29:31 +0000

    Aphid syntax highlighting.

---
+++ Components.Aphid.VSPackage/AphidLanguageService.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.Package;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Aphid.VSPackage
+{
+    public class AphidAuthoringScope : AuthoringScope
+    {
+        public override string GetDataTipText(int line, int col, out Microsoft.VisualStudio.TextManager.Interop.TextSpan span)
+        {
+            span = new Microsoft.VisualStudio.TextManager.Interop.TextSpan();
+            return null;
+        }
+
+        public override Declarations GetDeclarations(Microsoft.VisualStudio.TextManager.Interop.IVsTextView view, int line, int col, TokenInfo info, ParseReason reason)
+        {
+            return null;
+        }
+
+        public override Methods GetMethods(int line, int col, string name)
+        {
+            return null;
+        }
+
+        public override string Goto(Microsoft.VisualStudio.VSConstants.VSStd97CmdID cmd, Microsoft.VisualStudio.TextManager.Interop.IVsTextView textView, int line, int col, out Microsoft.VisualStudio.TextManager.Interop.TextSpan span)
+        {
+            span = new Microsoft.VisualStudio.TextManager.Interop.TextSpan();
+
+            return null;
+        }
+    }
+
+
+    public class AphidLanguageService : LanguageService
+    {
+        public override string GetFormatFilterList()
+        {
+            return "Aphid files (*.alx)\n*.alx\n";
+        }
+
+        public override LanguagePreferences GetLanguagePreferences()
+        {
+            return new LanguagePreferences()
+            {
+                //EnableCodeSense = true,
+                //AutoListMembers = true,
+                EnableFormatSelection = true,
+                //EnableMatchBraces = true,
+                //IndentSize = 4,
+                LineNumbers = true,
+                EnableCommenting = true,
+                //EnableShowMatchingBrace = true,
+                IndentStyle = IndentingStyle.Smart,
+            };
+        }
+
+        public override IScanner GetScanner(Microsoft.VisualStudio.TextManager.Interop.IVsTextLines buffer)
+        {
+            return new AphidScanner();
+        }
+
+        public override string Name
+        {
+            get { 
+                return "Aphid Language Service"; }
+        }
+
+        public override AuthoringScope ParseSource(ParseRequest req)
+        {
+            switch (req.Reason)
+            {
+                case ParseReason.MatchBraces:
+                    break;
+
+                case ParseReason.HighlightBraces:
+                    
+                    break;
+
+                default:
+                    break;
+            }
+
+            return new AphidAuthoringScope();
+        }
+    }
+}
---
+++ Components.Aphid.VSPackage/AphidScanner.cs
@@ -0,0 +1,135 @@
+﻿using Components.Aphid.Lexer;
+using Microsoft.VisualStudio.Package;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Aphid.VSPackage
+{
+    public class AphidScanner : IScanner
+    {
+        private int _index;
+        private List<AphidToken> _tokens;
+
+        public bool ScanTokenAndProvideInfoAboutIt(TokenInfo tokenInfo, ref int state)
+        {
+            if (_tokens.Count <= _index)
+            {
+                return false;
+            }
+
+            var t = _tokens[_index++];
+
+            tokenInfo.StartIndex = t.Index;
+            tokenInfo.EndIndex = t.Index + t.Lexeme.Length - 1;
+            tokenInfo.Type = TokenType.Text;
+            tokenInfo.Color = TokenColor.Text;            
+
+            switch (t.TokenType)
+            {
+                case AphidTokenType.String:
+                    tokenInfo.Type = TokenType.String;
+                    tokenInfo.Color = TokenColor.String;
+                    break;
+
+                case AphidTokenType.Number:
+                case AphidTokenType.HexNumber:
+                    tokenInfo.Type = TokenType.Literal;
+                    tokenInfo.Color = TokenColor.Number;
+                    break;
+
+                case AphidTokenType.Identifier:
+                    tokenInfo.Type = TokenType.Identifier;
+                    tokenInfo.Color = TokenColor.Identifier;
+                    break;
+
+                case AphidTokenType.breakKeyword:
+                case AphidTokenType.elseKeyword:
+                case AphidTokenType.falseKeyword:
+                case AphidTokenType.forKeyword:
+                case AphidTokenType.ifKeyword:
+                case AphidTokenType.inKeyword:
+                case AphidTokenType.nullKeyword:
+                case AphidTokenType.retKeyword:
+                case AphidTokenType.thisKeyword:
+                case AphidTokenType.trueKeyword: 
+                    tokenInfo.Type = TokenType.Keyword;
+                    tokenInfo.Color = TokenColor.Keyword;
+                    break;
+
+                case AphidTokenType.AdditionOperator:
+                case AphidTokenType.AndOperator:
+                case AphidTokenType.AssignmentOperator:
+                case AphidTokenType.AsyncOperator:
+                case AphidTokenType.BinaryAndOperator:
+                case AphidTokenType.BinaryOrOperator:
+                case AphidTokenType.ColonOperator:
+                case AphidTokenType.ComplementOperator:
+                case AphidTokenType.DecrementOperator:
+                case AphidTokenType.DivisionEqualOperator:
+                case AphidTokenType.DivisionOperator:
+                case AphidTokenType.EqualityOperator:
+                case AphidTokenType.ExistsOperator:
+                case AphidTokenType.functionOperator:
+                case AphidTokenType.GreaterThanOperator:
+                case AphidTokenType.GreaterThanOrEqualOperator:
+                case AphidTokenType.IncrementOperator:
+                case AphidTokenType.JoinOperator:
+                case AphidTokenType.LessThanOperator:
+                case AphidTokenType.LessThanOrEqualOperator:
+                case AphidTokenType.LoadLibraryOperator:
+                case AphidTokenType.LoadScriptOperator:
+                case AphidTokenType.MinusEqualOperator:
+                case AphidTokenType.MinusOperator:
+                case AphidTokenType.ModulusEqualOperator:
+                case AphidTokenType.ModulusOperator:
+                case AphidTokenType.MultiplicationEqualOperator:
+                case AphidTokenType.MultiplicationOperator:
+                case AphidTokenType.NotEqualOperator:
+                case AphidTokenType.NotOperator:
+                case AphidTokenType.OrEqualOperator:
+                case AphidTokenType.OrOperator:
+                case AphidTokenType.PipelineOperator:
+                case AphidTokenType.PlusEqualOperator:
+                case AphidTokenType.XorEqualOperator:
+                case AphidTokenType.XorOperator:
+                    tokenInfo.Type = TokenType.Operator;                    
+                    break;
+
+                case AphidTokenType.LeftBrace:
+                    tokenInfo.Trigger = TokenTriggers.MatchBraces;                    
+                    break;
+
+                case AphidTokenType.MemberOperator:
+                    tokenInfo.Trigger = TokenTriggers.MemberSelect;
+                    tokenInfo.Type = TokenType.Delimiter;                    
+                    break;
+
+                case AphidTokenType.LeftParenthesis:
+                    tokenInfo.Trigger = TokenTriggers.ParameterStart;
+                    break;
+
+                case AphidTokenType.RightParenthesis:
+                    tokenInfo.Trigger = TokenTriggers.ParameterEnd;
+                    break;
+            }
+
+            return true;
+        }
+
+        public void SetSource(string source, int offset)
+        {
+            _index = 0;
+            try
+            {
+                _tokens = new AphidLexer(source).GetTokens();
+            }
+            catch
+            {
+                _tokens = new List<AphidToken>();
+            }
+        }
+    }
+}
