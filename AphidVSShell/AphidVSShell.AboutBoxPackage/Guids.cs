20e60e6139efa9cac0e82c41572916c731976be7
John Leitch <john.leitch5@gmail.com>
2013-06-08 03:40:36 +0000

    Aphid VS shell.

---
+++ AphidVSShell/AphidVSShell.AboutBoxPackage/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace AphidVSShell.AboutBoxPackage
+{
+    static class GuidList
+    {
+        public const string guidAboutBoxPackagePkgString = "8f0310b2-1205-40d2-b636-89f030e375b5";
+        public const string guidAboutBoxPackageCmdSetString = "9583b111-0359-43db-bd1e-8f9c4b87c87e";
+
+        public static readonly Guid guidAboutBoxPackageCmdSet = new Guid(guidAboutBoxPackageCmdSetString);
+    };
+}
